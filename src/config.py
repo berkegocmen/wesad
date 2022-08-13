06e9e22b1b68b43af6e4834e6a25b1ab51e1e7c9
Berke Gocmen <berkegocmen@hotmail.com>
2022-08-13 22:56:49 +0300

    config.py update

--- src/config.py
+++ src/config.py
@@ -9,16 +9,18 @@ VALIDATION_FILE = '../data/S_csvs/S16.csv'
 training_files = {'tf_1': '../data/S_csvs/S2.csv',
                   'tf_2': '../data/S_csvs/S3.csv',
                   'tf_3': '../data/S_csvs/S4.csv',
-                  'tf_4': '../data/S_csvs/S16.csv',
-                  'tf_5': '../data/S_csvs/S17.csv',
+                  'tf_4': '../data/S_csvs/S5.csv',
+                  'tf_5': '../data/S_csvs/S6.csv',
                   'tf_6': '../data/S_csvs/S7.csv',
                   'tf_7': '../data/S_csvs/S8.csv',
                   'tf_8': '../data/S_csvs/S9.csv',
                   'tf_9': '../data/S_csvs/S10.csv',
                   'tf_10': '../data/S_csvs/S11.csv',
                   'tf_11': '../data/S_csvs/S13.csv',
-                  'tf_12': '../data/S_csvs/S14.csv',
-                  'tf_13': '../data/S_csvs/S15.csv'}
+                  }
 
-validation_files = {'vf_1': '../data/S_csvs/S5.csv',
-                    'vf_2': '../data/S_csvs/S6.csv'}
+validation_files = {'vf_1': '../data/S_csvs/S16.csv',
+                    'vf_2': '../data/S_csvs/S17.csv'}
+
+test_files = {'f_1': '../data/S_csvs/S14.csv',
+              'f_2': '../data/S_csvs/S15.csv'}
