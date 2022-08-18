893d5f61626a8373906912bc79363c9c50e729cb
Berke Gocmen <berkegocmen@hotmail.com>
2022-08-18 14:48:10 +0300

    refactor file  name

---
+++ src/train_lgbm_cf.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+python lgbm_cf.py --fold 0
+python lgbm_cf.py --fold 1
+python lgbm_cf.py --fold 2
+python lgbm_cf.py --fold 3
+python lgbm_cf.py --fold 4
+python lgbm_cf.py --fold 5
+python lgbm_cf.py --fold 6
