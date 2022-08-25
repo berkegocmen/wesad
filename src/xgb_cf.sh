44f77c71649e3c6280ab7fb2412e9ca54953794f
Berke Gocmen <berkegocmen@hotmail.com>
2022-08-25 10:20:47 +0300

    shell script for xgb cross validation

---
+++ src/xgb_cf.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+python xgbc.py --fold 0
+python xgbc.py --fold 1
+python xgbc.py --fold 2
+python xgbc.py --fold 3
+python xgbc.py --fold 4
+python xgbc.py --fold 5
+python xgbc.py --fold 6
